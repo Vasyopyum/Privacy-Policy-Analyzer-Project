c16fb9e21efe36d06aa88e2a573e95f03a672be1
Vasyopyum <bagrpcgame@gmail.com>
2026-04-06 21:51:01 +0600

    Удалил тестовый файл gemini_test.py из репозитория

--- gemini_test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-# Вставьте сюда свой ключ
-genai.configure(api_key="AIzaSyAjs7r4yuJNCDA6UdLl1L9hCdTOaaBVdiA")
-
-print("Список доступных моделей:")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
